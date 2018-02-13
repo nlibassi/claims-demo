e5e1d632427ddf7e1fdc38dc11281458817a7a05
Nick <nlibassi@gmail.com>
2018-02-13 07:59:23 +0300

    added basic user registration, finished Database chap (4) of M.G. tutorial

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,31 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, \
 SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import ValidationError, DataRequired, Email, \
+EqualTo
+from app.models import Insured
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(),
+        EqualTo('password')])
+    submit = SubmitField('Register')
+
+    #changed 'user' to 'insured' here for consistency with routes.py
+    def validate_username(self, username):
+        insured = Insured.query.filter_by(username=username.data).first()
+        if insured is not None:
+            raise ValidationError('Please use a different username')
+
+    def validate_email(self, email):
+        insured =  Insured.query.filter_by(email=email.data).first()
+        if insured is not None:
+            raise ValidationError('Please use a different email address')
